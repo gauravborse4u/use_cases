d68f267b45cd234842df73bc5061ba0bde1164fc
gauravborse4u <gauravborse4u@gmail.com>
2022-10-17 12:43:39 +0530

    commit

---
+++ javaConnect/src/test/java/com/example/javaConnection/javaConnect/JavaConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaConnection.javaConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
