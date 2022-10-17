6a2b3c4bcc06ffbc2500a2f43d82dcdcc4199048
gauravborse4u <gauravborse4u@gmail.com>
2022-10-17 16:41:41 +0530

    commit crud

---
+++ javaConnect/src/main/java/com/example/javaConnection/javaConnect/EmployeeRepo.java
@@ -0,0 +1,9 @@
+package com.example.javaConnection.javaConnect;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepo extends CrudRepository<EmployeeEntity, Integer> {
+
+	
+	
+}
