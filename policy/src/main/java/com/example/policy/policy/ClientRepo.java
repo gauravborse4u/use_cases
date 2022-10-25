918620156d009a3555849d44d23bc920cb4446d5
gauravborse4u <gauravborse4u@gmail.com>
2022-10-25 17:31:47 +0530

    commit

--- policy/src/main/java/com/example/policy/policy/ClientRepo.java
+++ policy/src/main/java/com/example/policy/policy/ClientRepo.java
@@ -2,26 +2,35 @@ package com.example.policy.policy;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepo extends CrudRepository<ClientsEntity, Integer> {
+public interface ClientRepo extends JpaRepository<ClientsEntity, Integer> {
 
 	@Query(value = "select * from insurance join clients on insurance.pid = clients.pid where cid=1;", nativeQuery = true)
 	public List<ClientsEntity> id1();
-	
+
 	@Query(value = "select * from insurance join clients on insurance.pid = clients.pid where cid=2;", nativeQuery = true)
 	public List<ClientsEntity> id2();
-	
+
 	@Query(value = "select * from insurance join clients on insurance.pid = clients.pid where cid=3;", nativeQuery = true)
 	public List<ClientsEntity> id3();
-	
+
 	@Query(value = "select * from insurance join clients on insurance.pid = clients.pid where cid=4;", nativeQuery = true)
 	public List<ClientsEntity> id4();
-	
+
 	@Query(value = "select * from insurance join clients on insurance.pid = clients.pid where cid=5;", nativeQuery = true)
 	public List<ClientsEntity> id5();
+
+	@Query(value = "select * from insurance join clients on insurance.pid = clients.pid where cid=6;", nativeQuery = true)
+	public List<ClientsEntity> id6();
+
+	@Query(value = "select * from insurance join clients on insurance.pid = clients.pid where cid=7;", nativeQuery = true)
+	public List<ClientsEntity> id7();
+
+	@Query(value = "select * from insurance join clients on insurance.pid = clients.pid where cid=8;", nativeQuery = true)
+	public List<ClientsEntity> id8();
 
 }
--- policy/src/main/java/com/example/policy/policy/ClientsController.java
+++ policy/src/main/java/com/example/policy/policy/ClientsController.java
@@ -1,9 +1,9 @@
 package com.example.policy.policy;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,36 +12,76 @@ public class ClientsController {
 	@Autowired
 	private ClientRepo cr;
 
+	@GetMapping("/{plan}/{amount}")
+	public Integer amount1(@PathVariable String plan, @PathVariable String amount) {
+
+		Integer Plan = Integer.parseInt(plan);
+		Integer Amount = Integer.parseInt(amount);
+
+		Integer percentage = 0;
+		if (Plan < 5) {
+			return Amount;
+		} else if (Plan == 5) {
+			percentage = 10 * Amount / 100;
+		} else {
+			percentage = 12 * Amount / 100;
+		}
+
+		return (Amount - percentage);
+
+	}
+
 	@GetMapping("/id=1")
 	public List<ClientsEntity> joincol() {
 
 		return cr.id1();
 
 	}
-	
+
 	@GetMapping("/id=2")
 	public List<ClientsEntity> joincol2() {
 
 		return cr.id2();
 
 	}
-	
+
 	@GetMapping("/id=3")
 	public List<ClientsEntity> joincol3() {
 
 		return cr.id3();
 
 	}
-	
+
 	@GetMapping("/id=4")
 	public List<ClientsEntity> joincol4() {
 
 		return cr.id4();
 
 	}
-	
+
 	@GetMapping("/id=5")
 	public List<ClientsEntity> joincol5() {
+
+		return cr.id5();
+
+	}
+
+	@GetMapping("/id=6")
+	public List<ClientsEntity> joincol6() {
+
+		return cr.id3();
+
+	}
+
+	@GetMapping("/id=7")
+	public List<ClientsEntity> joincol7() {
+
+		return cr.id4();
+
+	}
+
+	@GetMapping("/id=8")
+	public List<ClientsEntity> joincol8() {
 
 		return cr.id5();
 
