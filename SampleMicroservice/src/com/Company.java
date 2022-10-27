83b07832f03f0c082d0c542a8abab1f17896de4d
gauravborse4u <gauravborse4u@gmail.com>
2022-10-27 18:37:09 +0530

    commit for currency exchange

---
+++ SampleMicroservice/src/com/Company.java
@@ -0,0 +1,24 @@
+package com;
+
+public class Company {
+
+	String c_name;
+	String c_address;
+
+	public String getC_name() {
+		return c_name;
+	}
+
+	public void setC_name(String c_name) {
+		this.c_name = c_name;
+	}
+
+	public String getC_address() {
+		return c_address;
+	}
+
+	public void setC_address(String c_address) {
+		this.c_address = c_address;
+	}
+
+}
